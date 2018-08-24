053abf0868328be0601b741ea4c81253ca67d132
chiragrumak <chiragheir@gmail.com>
2018-08-24 01:50:39 -0500

    Update HeapSort.c

--- HeapSort.c
+++ HeapSort.c
@@ -35,3 +35,66 @@ int main(){
 	
 	return 0;
  }
+
+void heap_sort(int arr[], int size){
+	int max;
+	buildHeap(arr,size);
+	printf("\nThe heap is : ");
+	display(arr,size);
+	
+	while(size > 1){
+		max = del_root(arr,&size);
+		arr[size+1] = max;
+	}
+}
+
+void buildHeap(int arr[], int size){
+	int i;
+	for(i = size/2; i >= 1; i--)
+		restoreDown(arr,i,size);
+
+}
+
+int del_root(int arr[], int *size){
+	int max = arr[1];
+	arr[1] = arr[*size];
+	(*size)--;
+	restoreDown(arr,1,*size);
+	return max;
+}
+
+void restoreDown(int arr[], int i, int size){
+	int left = 2*i;
+	int right = left + 1;
+	int num = arr[i];
+	
+	while(right <=size){
+		if(num >= arr[left] && num >= arr[right]){
+			arr[i] = num;
+			return;
+		}
+		else if(arr[left] > arr[right]){
+			arr[i] = arr[left];
+			i = left;
+		}
+		else{
+			arr[i] = arr[right];
+			i = right;
+		}
+		left = 2 * i;
+		right = left + 1;
+	}
+	
+	if(left == size && num < arr[left]){
+		arr[i] = arr[left];
+		i = left;
+	}
+}
+
+void display(int arr[], int n){
+	int i;
+	
+	for(i=1; i <=n;i++)
+		printf("%2d", arr[i]);
+	printf("\n");
+}
