d6a3956c3c07bb3482fca5130dce272f682f2c20
chiragrumak <chiragheir@gmail.com>
2018-08-25 03:51:36 -0500

    Create RadixSort.c

---
+++ RadixSort.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+  int infor;
+  struct node *link;
+}*start = NULL;
+
+void radix_sort();
+int large_dig();
+int digit(int number, int k);
+
+int main(){
+  struct node, *tmp, *q;
+  int i,n,item;
+  printf("\n Enter the number of elements in the list : ");
+  scanf(" %d",&n);
+  
+  for(i=0; i<n; i++){
+    printf("\n Enter element %d: ",i+1);
+    scanf(" %d",&item);
+    
+    tmp = (struct node*) malloc (sizeof(struct node));
+    tmp->info = item;
+    tmp->link = NULL;
+    
+    if(start == NULL)
+      start = tmp;
+    else{
+      q = start;
+      
+      while(q->link != NULL){
+        q = q->link;
+      q->link = tmp;
+      }
+    }
+   
+  }
+
+
+}
+
+
