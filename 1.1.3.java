eecb4bda91d1ad1eb2899a6c33b7c47a3b21b872
chiragrumak <chiragheir@gmail.com>
2018-08-28 22:06:23 +0530

    Update 1.1.3.java

--- 1.1.3.java
+++ 1.1.3.java
@@ -1,26 +1,73 @@
+
 import java.util.*;
 
-public class Name2{
+public class HelloWorld {
 
-  public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the string: ");
-    String input = sc.next();
-    String temp = input;
-    
-    for(int i = 0; i < input.length(); i++){
-      int c = input.charAt(i);
-      
-      for(int j = i+1; j < input.length(); j++){
-        if(c==input.charAt(j)){
-          temp = input.substring(0,j) + input.substring(j+1);
-        }
-        input = temp;
-       }
-       System.out.println(input);
-     }
-    System.out.println(input);
-    sc.close();
- }
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the String: ");
+		String input = sc.nextLine();
+		//
+		// if (input.isEmpty())
+		// System.out.println("The string is empty.");
+		//
+		// for (int i = 0; i < input.length(); i++) {
+		// char c = input.charAt(i);
+		//
+		// for (int j = i + 1; j < input.length(); j++) {
+		//
+		// if (c == input.charAt(j)) {
+		// input = input.substring(0, j) + input.substring(j + 1);
+		// j--;
+		// }
+		// }
+		// }
+		// System.out.println(input);
+
+		removeDuplicates(input.toCharArray());
+
+		sc.close();
+
+	}
+
+	public static void removeDuplicates(char[] str) {
+
+		if (str == null)
+			return;
+
+		int len = str.length;
+
+		if (len < 2)
+			return;
+
+		int tail = 1;
+
+		for (int i = 1; i < len; ++i) {
+
+			int j;
+			//System.out.println(str);
+			for (j = 0; j < tail; ++j) {
+
+				if (str[i] == str[j])
+					break;
+
+			}
+
+			if (j == tail) {
+
+				str[tail] = str[i];
+
+				++tail;
+
+			}
+			String temp = String.valueOf(str);
+			System.out.println(temp);
+		}
+
+		str[tail] = 0;
+
+		System.out.println(str);
+	}
 
 }
