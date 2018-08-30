d13ac2a85602f425cd1265c058a57767417f6cb2
chiragrumak <chiragheir@gmail.com>
2018-08-30 22:47:00 +0530

    Update RadixSort.c

--- RadixSort.c
+++ RadixSort.c
@@ -47,4 +47,73 @@ int main(){
   return 0;
 }
 
+void radix_sort(){
+  int i,k,dig,least_sig, most_sig;
+  struct node *p, *rear[10], *front[10];
+  least_sig = 1;
+  most_sig = large_dig[start];
+  
+for(k = least_sig,  k <= most_sig; k++){
+   for(i=0; i<= 9; i++){
+    rear[i] = NULL;
+     front[i] = NULL;
+     }
+  
+  for(p = start;  p!=NULL; p=p->link){
+    dig = digit(p->info,k);
+    
+    if(front[dig] == NULL)
+        front[dig] = p;
+    else
+       rear[dig]->link = p;
+    rear[dig] = p;
+   }
+  
+  i = 0;
+  
+  while( front[i] == NULL)
+    i++;
+  
+  start = front[i];
+  
+  while( i < 9){
+      if(rear[i+1] != NULL)
+          rear[i]->link = front[i+1];
+    else
+         rear[i+1]  = rear[i];
+    i++;
+  }
+   
+  rear[9] ->link = NULL;
+}
+}
+
+int large_dig(){
+
+  struct node *p=str;
+  int large =0, ndig = 0;
+  
+  while( p!= NULL){
+    if(p->info > large)
+      large = p->info;
+    p =  p->link;
+  }
+  
+  while( large!=0){
+    ndig++;
+    large = large/10;
+  }
+return ndig;
+}
+
+int digit(int number, int k){
+  int digit, i;
+  
+  for( i=1; i <=k; i++){
+    digit = number % 10;
+    number = number/10;
+   }
+  return digit;
+}
+
 
