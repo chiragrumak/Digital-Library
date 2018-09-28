d98953e19b3839195e6cc671d166c3dbc4da8365
chiragrumak <chiragheir@gmail.com>
2018-09-28 12:16:35 +0530

    Update HeapSort.c

--- HeapSort.c
+++ HeapSort.c
@@ -1,10 +1,3 @@
-/**********************************************************************************
-					Heap Sort
-				Author    : Vineet kumar
-				Reference : Data Structures through C in Depth
-				Start Date: 23/08/2018
-***********************************************************************************/
-
 #include<stdio.h>
 #include<stdlib.h>
 #define MAX 100
@@ -22,8 +15,8 @@ int main(){
 	scanf(" %d",&n);
 	printf("\n Reached till here! n = %d",n);	
 
-	for(i=0;i<n;i++){
-		printf("\nEnter element %d: ",i+1);
+	for(i=1;i<=n;i++){
+		printf("\nEnter element %d: ",i);
 		scanf(" %d",&arr[i]);	
 	}
 	
@@ -67,6 +60,118 @@ void restoreDown(int arr[], int i, int size){
 	int left = 2*i;
 	int right = left + 1;
 	int num = arr[i];
+	int temp;
+
+//	printf("arr[%d] = %d , arr[%d] = %d \n", left,arr[left],right,arr[right]);
+	
+	while(right <=size){
+		if(num >= arr[left] && num >= arr[right]){
+			arr[i] = num;
+			return;
+		}
+		else if(arr[left] > arr[right]){
+			temp = arr[i];			
+			arr[i] = arr[left];
+			arr[left] = temp;			
+			i = left;
+		}
+		else{
+			temp = arr[i];
+			arr[i] = arr[right];
+			arr[right] = temp;
+			i = right;
+		}
+		left = 2 * i;
+		right = left + 1;
+	//printf("\n Reached till here");
+	}
+	
+	if(left == size && num < arr[left]){
+		temp = arr[i];
+		arr[i] = arr[left];
+		arr[right] = temp;
+		i = left;
+	}
+//	printf("\n Array at this moment = ");
+	//display(arr,size);
+}
+
+void display(int arr[], int n){
+	int i;
+	
+	for(i=1; i <=n;i++)
+		printf(" %2d ", arr[i]);
+	printf("\n");
+}
+
+// Backed Up Old Code:
+
+/*
+
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 100
+
+void heap_sort(int arr[], int size);
+void buildHeap(int arr[], int size);
+int del_root(int arr[], int *size);
+void restoreDown(int arr[], int i, int size);
+void display(int arr[], int n);
+
+int main(){
+	int i,n,arr[MAX];
+	
+	printf("\nEnter the number of elements: ");
+	scanf(" %d",&n);
+	printf("\n Reached till here! n = %d",n);	
+
+	for(i=1;i<=n;i++){
+		printf("\nEnter element %d: ",i);
+		scanf(" %d",&arr[i]);	
+	}
+	
+	printf("\nEnterd list is: \n" );
+	display(arr,n);
+	heap_sort(arr,n);
+	printf("\nSorted List is : \n");
+	display(arr,n);
+	
+	return 0;
+ }
+
+void heap_sort(int arr[], int size){
+	int max;
+	buildHeap(arr,size);
+	printf("\nThe heap is : ");
+	display(arr,size);
+	
+	while(size > 1){
+		max = del_root(arr,&size);
+		arr[size+1] = max;
+	}
+}
+
+void buildHeap(int arr[], int size){
+	int i;
+	for(i = size/2; i >= 1; i--)
+		restoreDown(arr,i,size);
+
+}
+
+int del_root(int arr[], int *size){
+	int max = arr[1];
+	arr[1] = arr[*size];
+	(*size)--;
+	restoreDown(arr,1,*size);
+	return max;
+}
+
+void restoreDown(int arr[], int i, int size){
+	int left = 2*i;
+	int right = left + 1;
+	int num = arr[i];
+
+	printf("arr[%d] = %d , arr[%d] = %d \n", left,arr[left],right,arr[right]);
 	
 	while(right <=size){
 		if(num >= arr[left] && num >= arr[right]){
@@ -89,12 +194,18 @@ void restoreDown(int arr[], int i, int size){
 		arr[i] = arr[left];
 		i = left;
 	}
+	printf("\n Array at this moment = ");
+	display(arr,size);
 }
 
 void display(int arr[], int n){
 	int i;
 	
 	for(i=1; i <=n;i++)
-		printf("%2d", arr[i]);
+		printf(" %2d ", arr[i]);
 	printf("\n");
 }
+
+
+*/
+
